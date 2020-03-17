f3bd802bea3fe206aa56f4d163912e09cfb65313
mattosaurus <mattosaurus@outlook.com>
2020-03-17 11:30:52 +0000

    Add project files.

---
+++ CampaignCalendar/Extensions/ListExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignCalendar.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
+        static public IEnumerable<T> SelfAndTraverse<T>(this T source, Func<T, T> TraverseBy)
+        {
+            if (source == null)
+                yield break;
+
+            yield return source;
+
+            var children = TraverseBy(source);
+            if (children == null)
+                yield break;
+
+            foreach (var child in children.SelfAndTraverse(TraverseBy))
+            {
+                yield return child;
+            }
+        }
+
+        static public IEnumerable<T> Traverse<T>(this T source, Func<T, T> TraverseBy)
+        {
+            if (source == null)
+                yield break;
+
+            var children = TraverseBy(source);
+            if (children == null)
+                yield break;
+
+            foreach (var child in children.SelfAndTraverse(TraverseBy))
+            {
+                yield return child;
+            }
+        }
+    }
+}
---
+++ CampaignCalendar/Models/CampaignModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignCalendar.Models
+{
+    public class CampaignModel
+    {
+        public int Id { get; set; }
+
+        public CampaignStatus Status { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum CampaignStatus
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
